899eecdb6151e201c2b08f22e6125e3e0abeb52a
ngayam <ngayam@QTSH19.QUADRATICS.LOCAL>
2020-01-22 12:30:22 +0530

    improvements

--- src/main/java/com/qts/business/controller/ServletDate.java
+++ src/main/java/com/qts/business/controller/ServletDate.java
@@ -1,11 +1,19 @@
 package com.qts.business.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.qts.business.vo.Business;
+
 
 /**
  * Servlet implementation class ServletDate
@@ -15,20 +23,11 @@ public class ServletDate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public ServletDate() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -36,27 +35,23 @@ public class ServletDate extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		JSONObject reqJson;
-		JSONObject respJson = new JSONObject();
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//JsonObject reqJson;
+		JsonObject respJson = new JsonObject();
 		try {
-			// get Reader from request
-			Reader reqReader = req.getReader();
-			JSONParser parser = new JSONParser();
-			// parse our request to json
-			reqJson = (JSONObject) parser.parse(reqReader);
-		} catch (ParseException | ClassCastException ex) {
-			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			respJson.put("answer", "Something bad happened");
-			try (PrintWriter out = resp.getWriter()) {
-				out.println(respJson.toJSONString());
+			//JsonParser parser = new JsonParser();
+			//reqJson = (JsonObject) parser.parse(request.getReader()); // parse our request to json
+			Gson gson = new Gson();
+			Business business = gson.fromJson(request.getReader(), Business.class);
+			System.out.println(business);
+		} catch (JsonParseException | ClassCastException ex) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			respJson.addProperty("answer", "Something bad happened");
+			try (PrintWriter out = response.getWriter()) {
+				out.println(respJson.toString());
 				out.flush();
 			}
 		}
-
 		doGet(request, response);
 	}
 
